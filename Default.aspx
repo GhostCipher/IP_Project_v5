b14112c73d1997a740ce1322c43dc798f20e1f70
GhostCipher <47286235+GhostCipher@users.noreply.github.com>
2021-04-22 23:46:59 -0500

    we did things

---
+++ Default.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="wtf" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Sign Out" />
+</asp:Content>
+
---
+++ Site.master.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+
+public partial class MasterPage : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        // Check for Authentication Cookie
+        FormsAuthenticationTicket ticket;
+        if (Request.Cookies[FormsAuthentication.FormsCookieName] != null)
+        {
+            ticket = FormsAuthentication.Decrypt(Request.Cookies[FormsAuthentication.FormsCookieName].Value);
+            UserNameTxtBox.Text = ticket.Name;
+        }
+        else UserNameTxtBox.Text = null;
+    }
+
+    protected void LoginStatus1_LoggingOut(object sender, LoginCancelEventArgs e)
+    {
+      
+    }
+
+    protected void LoginStatus1_LoggedOut(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void TextBox1_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+}
